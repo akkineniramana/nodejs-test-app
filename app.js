6a0ce6de2167d459fc0ba023f8d22071d0ea4384
akkineniramana <venkataramana_akkineni@gap.com>
2017-08-26 12:36:48 -0700

    add start script

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.env.PORT || "3000";
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -37,6 +37,6 @@ app.get('/about', (request, response) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server Listening on Port 3000');
 })
